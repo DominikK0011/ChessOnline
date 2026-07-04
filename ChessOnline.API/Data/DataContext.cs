13b834051dbe7e62b12ed9f9ed7002ad0a79e676
DominikK0011 <dominik.koza00@gmail.com>
2026-07-04 14:46:54 +0200

    Add EF Core with PostgreSQL and initial User model
    Integrated Entity Framework Core with PostgreSQL provider. Added DataContext and User model. Generated initial migration for Users table. Updated Program.cs for DI and DB config.

---
+++ ChessOnline.API/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ChessOnline.API.Models;
+
+namespace ChessOnline.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ChessOnline.API/Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace ChessOnline.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
+        public DateTime Birthday { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+
+    }
+}
